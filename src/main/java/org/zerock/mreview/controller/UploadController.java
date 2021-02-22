c10f147e753654b66a2a63631fe7e1fe4982e65f
shsha4 <shsha4@naver.com>
2021-02-22 14:14:48 +0900

    review crud 화면 구현, starrr 라이브러리 별점 기능 구현, 이미지 목록 클릭시 원본 이미지 모달 생성

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -82,7 +82,7 @@ public class UploadController {
 
     //업로드 한 이미지 화면에 출력
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName) {
+    public ResponseEntity<byte[]> getFile(String fileName, String size) {
         ResponseEntity<byte[]> result = null;
 
         try {
@@ -90,6 +90,10 @@ public class UploadController {
             log.info("fileName : " + srcFileName);
 
             File file = new File(uploadPath + File.separator + srcFileName);
+
+            if(size != null && size.equals("1")) {
+                file = new File(file.getParent(), file.getName().substring(2));
+            }
             log.info("file : " + file);
 
             HttpHeaders header = new HttpHeaders();
